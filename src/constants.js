03bc945747f5c57bd85d63ff281cd284209330e3
Isabella Ramirez <irborrero@gmail.com>
2020-02-05 16:45:00 -0500

    http-https

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://wbdv-generic-server.herokuapp.com/api/ramirezborrero/courses"
+export const API_URL = "https://wbdv-generic-server.herokuapp.com/api/ramirezborrero/courses"
 export const DEFAULT_COURSE_TITLE = "New Course"
 export const DEFAULT_SECTION_TITLE = "New Section"
