313cb19c608131ce0715329f37d6f4fbf15b986f
Isabella Ramirez <irborrero@gmail.com>
2020-02-09 12:31:39 -0500

    Topic Service Implemented

---
+++ src/services/TopicService.js
@@ -0,0 +1,45 @@
+import {API_URL} from "../constants";
+
+//TODO: ADD TOPIC
+export const createTopic = (lessonId) =>
+    fetch(`${API_URL}/lessons/${lessonId}/topics`, {
+        method: "POST",
+        body: JSON.stringify({title: "New Lesson"}),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+export const findTopicsForLesson = (lessonId) =>
+    fetch(`${API_URL}/lessons/${lessonId}/topics`)
+        .then(response => response.json())
+
+
+export const deleteTopic = (topicId) =>
+    fetch(`${API_URL}/topics/${topicId}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json())
+
+
+export const findTopic = async (topicId) => {
+    const response = await fetch(`${API_URL}/topics/${topicId}`)
+    return await  response.json()
+}
+
+
+export const updateTopic = async (topicId, topic) => {
+    return fetch(`${API_URL}/topics/${topicId}`, {
+        method: 'PUT',
+        body: JSON.stringify(topic),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+}
+
+
+export default {
+}
